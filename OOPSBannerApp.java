98bb4c0657aa6b7a1fdbc7a42c96ca660b788361
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-02-24 18:41:15 +0530

    UC3 string implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,21 @@
 /**
- * OOPSBannerApp UC2 - Print Banner
+* OOPSBannerApp UC3 - String.join()
  * This version prints the OOPS banner using ASCII art.
- * Each letter is designed to be ~9 characters wide and 7 lines high.
+ * It refactors the code to use String.join() instead of the '+' operator
+ * to improve memory efficiency and reduce intermediate object creation.
  *
  * @author Developer
- * @version 2.1
+ * @version 3.0
  */
-public class OOPSBannerApp {
+ public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        // Line 1
-        System.out.println("  *****  " + "  " + "  *****  " + "  " + " ******  " + "  " + "  *****  ");
-
-        // Line 2
-        System.out.println(" *     * " + "  " + " *     * " + "  " + " *     * " + "  " + " *     * ");
-
-        // Line 3
-        System.out.println(" *     * " + "  " + " *     * " + "  " + " *     * " + "  " + " *       ");
-
-        // Line 4
-        System.out.println(" *     * " + "  " + " *     * " + "  " + " ******  " + "  " + "  *****  ");
-
-        // Line 5
-        System.out.println(" *     * " + "  " + " *     * " + "  " + " *       " + "  " + "       * ");
-
-        // Line 6
-        System.out.println(" *     * " + "  " + " *     * " + "  " + " *       " + "  " + " *     * ");
-
-        // Line 7
-        System.out.println("  *****  " + "  " + "  *****  " + "  " + " *       " + "  " + "  *****  ");
+        System.out.println(String.join(" ", "  *****  ", "  *****  ", " ******  ", "  *****  "));
+        System.out.println(String.join(" ", " *     * ", " *     * ", " *     * ", " *     * "));
+        System.out.println(String.join(" ", " *     * ", " *     * ", " *     * ", " *       "));
+        System.out.println(String.join(" ", " *     * ", " *     * ", " ******  ", "  *****  "));
+        System.out.println(String.join(" ", " *     * ", " *     * ", " *       ", "       * "));
+        System.out.println(String.join(" ", " *     * ", " *     * ", " *       ", " *     * "));
+        System.out.println(String.join(" ", "  *****  ", "  *****  ", " *       ", "  *****  "));
     }
 }
