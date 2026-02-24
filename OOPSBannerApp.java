c0e655390baba059d85d4efb80c1628b4118e284
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-02-24 18:26:00 +0530

    UC2 print OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,33 @@
 /**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
+ * OOPSBannerApp UC2 - Print Banner
+ * This version prints the OOPS banner using ASCII art.
+ * Each letter is designed to be ~9 characters wide and 7 lines high.
  *
  * @author Developer
- * @version 1.0
+ * @version 2.1
  */
 public class OOPSBannerApp {
-    
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Line 1
+        System.out.println("  *****  " + "  " + "  *****  " + "  " + " ******  " + "  " + "  *****  ");
+
+        // Line 2
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *     * " + "  " + " *     * ");
+
+        // Line 3
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *     * " + "  " + " *       ");
+
+        // Line 4
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " ******  " + "  " + "  *****  ");
+
+        // Line 5
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *       " + "  " + "       * ");
+
+        // Line 6
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *       " + "  " + " *     * ");
+
+        // Line 7
+        System.out.println("  *****  " + "  " + "  *****  " + "  " + " *       " + "  " + "  *****  ");
     }
 }
